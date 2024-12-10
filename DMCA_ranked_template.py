54b8be108afd0e8919eaf27cd2cfb0e6cc441342
seedcrack <j5tanabodee@gmail.com>
2024-12-10 21:28:50 +0700

    DMCA'd maps finder

--- DMCA_ranked_template.py
+++ DMCA_ranked_template.py
@@ -11,6 +11,7 @@ def fetch_beatmapset(beatmap_id):
         return api.beatmapset(beatmap_id)
     except:
         pass
+        
 for i in range(0, 0): # change the range to whatever you want
     try:
         beatmapset = fetch_beatmapset(i)
@@ -22,11 +23,13 @@ for i in range(0, 0): # change the range to whatever you want
                     print(f"DL_disabled : https://osu.ppy.sh/beatmapsets/{beatmapset.id}/  ")
                 else:
                     print(f"Missing_content : https://osu.ppy.sh/beatmapsets/{beatmapset.id}/")
-            elif (api.beatmapset(i).ranked).value == 1 or 3:
+                    
+            map_status = api.beatmapset(i).ranked).value
+            if map_statuse == 1 or map_status == 3:
                 ranked_fine.append(beatmapset.id)
-            elif (api.beatmapset(i).ranked).value == 2:
+            elif map_status == 2:
                 approved_fine.append(beatmapset.id)
-            elif (api.beatmapset(i).ranked).value == 4:
+            elif map_status == 4:
                 loved_fine.append(beatmapset.id)
     except: # ctrl + C to interrupt the process in the cmd
         print(f"the currrent ID is {i}")
